42aec7a104f741e705bdabe5f8b190c80f1be5a5
dislersd <dislersd@gmail.com>
2020-01-19 18:16:58 -0800

    fetching sessions with useQuery hook

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 
 const Home = () => {
+  const { loading, data } = useQuery(FETCH_SESSIONS_QUERY);
+
+  if (data) {
+    console.log(data);
+  }
+
   return <div>Home Page</div>;
 };
+
+const FETCH_SESSIONS_QUERY = gql`
+  {
+    getSessions {
+      id
+      date
+      attendees
+    }
+  }
+`;
 
 export default Home;
